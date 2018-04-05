0d97c3ce707f43f6650d32d43dac2d7549bed864
liang.peng <1427985322@qq.com>
2018-04-06 01:42:06 +0800

    spring cloud eureka

--- blog-server/src/main/java/com/blog/Application.java
+++ blog-server/src/main/java/com/blog/Application.java
@@ -2,6 +2,7 @@ package com.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+                .web(true).run(args);
     }
 }
